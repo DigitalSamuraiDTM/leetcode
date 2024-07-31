3a78f74d733d0f9e49736c7788b6d0efc9ec78d7
Andrew <nugaev.andrei@gmail.com>
2024-08-01 00:16:10 +0300

    20-valid-parentheses

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,43 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.NullPointerException
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println(isValid("()"))
+    println(isValid("()[]{}"))
+    println(isValid("(]"))
+    println(isValid("(((())))"))
+    println(isValid("()(()){}{{[}]}"))
+    println(isValid("(("))
+    println(isValid("){"))
+}
+
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+val skobkaMap = mapOf(')' to '(', ']' to '[', '}' to '{')
+val skobkaOpenSet = setOf('(','[','{')
+var skobkaStack = ArrayDeque<Char>()
+fun isValid(s: String): Boolean {
+    if (s.length == 1) return false
+    s.forEach { char ->
+        if (char in skobkaOpenSet) {
+            skobkaStack.addLast(char)
+        } else {
+            val reversedSkobka = skobkaMap[char] ?: throw NullPointerException("Task contains only skobka")
+            if (reversedSkobka == skobkaStack.lastOrNull()) {
+                skobkaStack.removeLast()
+            } else {
+                return false
+            }
+        }
+    }
+    return skobkaStack.isEmpty()
 }
