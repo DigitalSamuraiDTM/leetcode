94d107db323c642858158f4170bf29b033c65db9
Andrew <nugaev.andrei@gmail.com>
2024-09-10 01:31:03 +0300

    28-find-the-index-oof-the-first-occurence-in-a-string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,38 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(strStr("mississippi","pi")) // 9
+    println(strStr("qweeerty","eee")) // 2
+    println(strStr("mississippi","issip")) // 4
+    println(strStr("sadbutsad","sad")) // 0
+    println(strStr("leetcode","leeto"))// -1
+    println(strStr("qwerty","r")) // 3
+    println(strStr("qwerty","rerty")) // -1
+    println(strStr("qwerty","y")) // 5
+    println(strStr("qqqqqqqqqqqqqqqqq","qqqqq")) // 0
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun strStr(haystack: String, needle: String): Int {
+    var index = 0
+    var out = 0
+    var i = 0
+    while(i != haystack.length) {
+        val char = haystack[i]
+        if (char == needle[index]) {
+            if (index == 0) {
+                out = i
+            }
+            index++
+        } else {
+            if (index != 0) {
+                i = out + 1
+                index = 0
+                out = 0
+                continue
+            }
+        }
+        if (index == needle.length) {
+            return out
+        }
+        i++
+    }
+    return -1
 }
