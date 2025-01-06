e57842fd131ee81f912ad8d9c4adfc4a8257aec9
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-01-06 20:21:52 +0300

    104. Maximum Depth of Binary Tree

--- Main.go
+++ Main.go
@@ -1,5 +1,70 @@
 package main
 
 func main() {
-	println("Hello World!")
+
+	exampleNode := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val:   9,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val:   15,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   7,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+
+	exampleOnlyRight := &TreeNode{
+		Val:  4,
+		Left: nil,
+		Right: &TreeNode{
+			Val:  5,
+			Left: nil,
+			Right: &TreeNode{
+				Val:  6,
+				Left: nil,
+				Right: &TreeNode{
+					Val:   7,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		},
+	}
+
+	println(maxDepth(exampleNode))
+	println(maxDepth(exampleOnlyRight))
+	println(maxDepth(nil))
+}
+
+func maxDepth(root *TreeNode) int {
+	max := 1
+	if root == nil {
+		return 0
+	}
+
+	maxLeft := maxDepth(root.Left)
+	maxRight := maxDepth(root.Right)
+	if maxLeft > maxRight {
+		max += maxLeft
+	} else {
+		max += maxRight
+	}
+	return max
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
