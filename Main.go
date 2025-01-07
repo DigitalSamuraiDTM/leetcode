36f676c70e4a9f0622e40b77dcea7dd4f0afdd47
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-01-07 19:50:54 +0300

    217. Contains duplicates

--- Main.go
+++ Main.go
@@ -1,5 +1,21 @@
 package main
 
 func main() {
-	println("Hello World!")
+	println(containsDuplicate([]int{1, 2, 3, 4}))                         // false
+	println(containsDuplicate([]int{1, 2, 3, 4, 1}))                      // true
+	println(containsDuplicate([]int{1, 2, 3, 4, -1, 5}))                  // false
+	println(containsDuplicate([]int{1, 2, 3, 4, -1, -2, -3, -4, -5, -1})) // true
+	println(containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}))       // true
+}
+
+func containsDuplicate(nums []int) bool {
+	checker := make(map[int]bool)
+	for _, value := range nums {
+		if checker[value] == true {
+			return true
+		} else {
+			checker[value] = true
+		}
+	}
+	return false
 }
