6aef49f080a39410228034f65d8a0cc675794832
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-01-09 00:38:25 +0300

    21. Merge Two Sorted Lists

--- Main.go
+++ Main.go
@@ -1,5 +1,72 @@
 package main
 
 func main() {
-	println("Hello World!")
+	list1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+	list2 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+	case1 := mergeTwoLists(list1, list2)
+	out(case1)
+}
+
+func out(listNode *ListNode) {
+	if listNode == nil {
+		return
+	}
+	println(listNode.Val)
+	out(listNode.Next)
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	// finish condition
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	// fast forward first list
+	if list1 != nil && list2 == nil {
+		return &ListNode{
+			Val:  list1.Val,
+			Next: mergeTwoLists(list1.Next, nil),
+		}
+	}
+	// fast forward second list
+	if list1 == nil && list2 != nil {
+		return &ListNode{
+			Val:  list2.Val,
+			Next: mergeTwoLists(list2.Next, nil),
+		}
+	}
+	// sorting
+	if list1.Val < list2.Val {
+		return &ListNode{
+			Val:  list1.Val,
+			Next: mergeTwoLists(list1.Next, list2),
+		}
+	} else {
+		return &ListNode{
+			Val:  list2.Val,
+			Next: mergeTwoLists(list1, list2.Next),
+		}
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
