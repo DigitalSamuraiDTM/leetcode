57dad55d1dd19de50622ade8c5b49435b690a744
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-01-26 19:30:24 +0300

    136. Single Number

--- Main.go
+++ Main.go
@@ -1,5 +1,32 @@
 package main
 
 func main() {
-	println("Hello World!")
+	e1 := []int{2, 2, 1}
+	println(singleNumber(e1))
+
+	e2 := []int{4, 1, 2, 1, 2}
+	println(singleNumber(e2))
+
+	e3 := []int{1}
+	println(singleNumber(e3))
+
+	e4 := []int{1, 2, 3, 4, 3, 2, 1}
+	println(singleNumber(e4))
+}
+
+func singleNumber(nums []int) int {
+	checkedMap := make(map[int]struct{})
+	for _, value := range nums {
+		_, isExist := checkedMap[value]
+		if isExist {
+			delete(checkedMap, value)
+		} else {
+			checkedMap[value] = struct{}{}
+		}
+	}
+	out := 0
+	for key, _ := range checkedMap {
+		out = key
+	}
+	return out
 }
