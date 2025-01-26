7d6073968f6713b980ad39da815a3fd2ea891b44
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-01-26 21:04:11 +0300

    101. Symmetric tree

--- Main.go
+++ Main.go
@@ -1,5 +1,82 @@
 package main
 
 func main() {
-	println("Hello World!")
+	e1 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+	println(isSymmetric(e1))
+
+	e2 := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:  2,
+			Left: nil,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val:  2,
+			Left: nil,
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+	}
+	println(isSymmetric(e2))
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	var leftNode *TreeNode = root.Left
+	var rightNode *TreeNode = root.Right
+	return checkSubtree(leftNode, rightNode)
+}
+
+func checkSubtree(leftTree *TreeNode, rightTree *TreeNode) bool {
+	if leftTree == nil && rightTree != nil ||
+		leftTree != nil && rightTree == nil {
+		return false
+	}
+	if leftTree == nil && rightTree == nil {
+		return true
+	}
+	// both tree exist...
+	if leftTree.Val == rightTree.Val {
+		leftRightCheck := checkSubtree(leftTree.Left, rightTree.Right)
+		if leftRightCheck {
+			rightLeftCheck := checkSubtree(leftTree.Right, rightTree.Left)
+			return rightLeftCheck
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
