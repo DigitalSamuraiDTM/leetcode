b89ee395f263b159eba8f206648d03fcc5daed64
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-02-08 03:09:57 +0300

    125. Valid Palindrome

--- Main.go
+++ Main.go
@@ -1,5 +1,40 @@
 package main
 
+import "unicode"
+
 func main() {
-	println("Hello World!")
+	println(isPalindrome("0P"))
+	println(isPalindrome("aaa"))
+	println(isPalindrome(".,"))
+	println(isPalindrome(".,."))
+	println(isPalindrome("A man, a plan, a canal: Panama"))
+	println(isPalindrome("race a car"))
+	println(isPalindrome(" "))
+}
+
+func isPalindrome(s string) bool {
+	leftIndex := 0
+	rightIndex := len(s) - 1
+	for rightIndex > leftIndex {
+		l := rune(s[leftIndex])
+		r := rune(s[rightIndex])
+		isLL := unicode.IsLetter(l) || unicode.IsNumber(l)
+		isRL := unicode.IsLetter(r) || unicode.IsNumber(r)
+		if isLL && isRL {
+			if unicode.ToLower(l) != unicode.ToLower(r) {
+				return false
+			}
+			leftIndex++
+			rightIndex--
+		} else {
+			if !isLL {
+				leftIndex++
+			}
+			if !isRL {
+				rightIndex--
+			}
+		}
+	}
+
+	return true
 }
