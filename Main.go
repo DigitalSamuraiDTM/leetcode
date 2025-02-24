3eb4721ae676ed1e224fb6a71e68c3e51c08d134
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-02-25 00:54:21 +0300

    145. Binary Tree Postorder Traversal

--- Main.go
+++ Main.go
@@ -1,5 +1,37 @@
 package main
 
 func main() {
-	println("Hello World!")
+	// solved without testing
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	out := []int{}
+	if root != nil {
+		if root.Left != nil {
+			rekykler(root.Left, &out)
+		}
+		if root.Right != nil {
+			rekykler(root.Right, &out)
+		}
+		out = append(out, root.Val)
+	}
+	return out
+}
+
+func rekykler(node *TreeNode, out *[]int) {
+	if node != nil {
+		if node.Left != nil {
+			rekykler(node.Left, out)
+		}
+		if node.Right != nil {
+			rekykler(node.Right, out)
+		}
+		*out = append(*out, node.Val)
+	}
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
