3b0ed3905a54e1eea55f20a6b91a85104a92718c
Andrew Nugaev <digit-samurai@yandex-team.ru>
2025-03-05 01:45:55 +0300

    205. Isomorphic Strings

--- Main.go
+++ Main.go
@@ -1,5 +1,36 @@
 package main
 
 func main() {
-	println("Hello World!")
+	println(isIsomorphic("badc", "baba"))
+	//println(isIsomorphic("", ""))
+	//println(isIsomorphic("a", "b"))
+	//println(isIsomorphic("paper", "title"))
+	//println(isIsomorphic("egg", "add"))
+	//println(isIsomorphic("foo", "bar"))
+}
+
+func isIsomorphic(s string, t string) bool {
+	mirrorL := map[uint8]uint8{}
+	mirrorR := map[uint8]uint8{}
+	for i := 0; i < len(s); i++ {
+		left := s[i]
+		right := t[i]
+
+		if mirrorValue, isExist := mirrorL[left]; !isExist {
+			mirrorL[left] = right
+		} else {
+			if mirrorValue != right {
+				return false
+			}
+		}
+
+		if mirrorValue, isExist := mirrorR[right]; !isExist {
+			mirrorR[right] = left
+		} else {
+			if mirrorValue != left {
+				return false
+			}
+		}
+	}
+	return true
 }
