e3cbce4a585ff3fa185b012f5d544959762c12cd
Nugaev Andrey <nugaev.andrey@rwb.ru>
2025-12-05 20:51:34 +0300

    70. Climbing Stairs

--- Main.go
+++ Main.go
@@ -1,5 +1,25 @@
 package main
 
 func main() {
-	println("Hello World!")
+	println(climbStairs(6))
+	println(climbStairs(7))
+	println(climbStairs(8))
+	println(climbStairs(9))
+}
+
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	var ne = 2
+	if n == ne {
+		return ne
+	}
+	var i = 3
+	var current = 1 + ne
+	for n != i {
+		current, ne = ne+current, current
+		i++
+	}
+	return current
 }
