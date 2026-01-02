72d852282a35395e8fa9e73c33816a6e81036242
Nugaev Andrey <nugaev.andrey@rwb.ru>
2026-01-02 03:06:40 +0300

    290. Word Pattern

--- Main.go
+++ Main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"strings"
+)
+
 func main() {
-	println("Hello World!")
+	//println(wordPattern("abba", "dog cat cat dog"))
+	//println(wordPattern("abba", "dog cat cat fish"))
+	//println(wordPattern("aaaa", "dog cat cat dog"))
+	//println(wordPattern("abba", "dog dog dog dog"))
+	println(wordPattern("aaa", "aa aa aa aa"))
+}
+
+func wordPattern(pattern string, s string) bool {
+	patternCharToWord := make(map[uint8]string)
+	patternWordToChar := make(map[string]uint8)
+	splitedWords := strings.Split(s, " ")
+	if len(splitedWords) != len(pattern) {
+		return false
+	}
+	for index, word := range splitedWords {
+		char := pattern[index]
+		patternedWord, isWordExist := patternCharToWord[char]
+		patternedChar, isCharExist := patternWordToChar[word]
+		if !isWordExist && !isCharExist {
+			patternCharToWord[char] = word
+			patternWordToChar[word] = char
+			continue
+		}
+		if patternedWord != word || patternedChar != char {
+			return false
+		}
+	}
+	return true
 }
